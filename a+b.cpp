85846162430cb53ea64c6f242ef30092666fdcec
Bohdanrstr <bogdanrstr@gmail.com>
2025-09-28 19:59:40 +0300

    First commit

---
+++ a+b.cpp
@@ -0,0 +1,16 @@
+#include <iostream>;
+
+using namespace std;
+
+
+int main(){
+    int a , b;
+
+    cout << "введіть а " << endl;
+    cin >> a;
+    cout << "Введіть b " << endl;
+    cin >> b;
+    cout << a+b << endl;
+
+
+}
